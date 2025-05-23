1eb32b9518a44377dfd793869afb43ed4aefc5ec
Z-HongYuan <1939218189@qq.com>
2025-05-24 01:05:08 +0800

    添加GameModeBase

---
+++ Source/One/Common/Gameplay/GameMode/OneGameModeBase.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+
+#include "OneGameModeBase.h"
---
+++ Source/One/Common/Gameplay/GameMode/OneGameModeBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONE_API AOneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
