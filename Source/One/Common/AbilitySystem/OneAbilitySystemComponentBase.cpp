5413e932281bff8d6a54a0f55c0c59dd8e0ec1d9
Z-HongYuan <1939218189@qq.com>
2025-05-25 13:52:31 +0800

    完善ASC的输入设置和GiveAbility的设置

--- Source/One/Common/AbilitySystem/OneAbilitySystemComponentBase.cpp
+++ Source/One/Common/AbilitySystem/OneAbilitySystemComponentBase.cpp
@@ -3,32 +3,87 @@
 
 #include "OneAbilitySystemComponentBase.h"
 
+#include "AbilitySystemLog.h"
+#include "Ability/OneGameplayAbilityBase.h"
 
-// Sets default values for this component's properties
+
 UOneAbilitySystemComponentBase::UOneAbilitySystemComponentBase()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	// SetIsReplicated(true); //选择设置是否复制
+}
 
-	// ...
+FGameplayAbilitySpecHandle UOneAbilitySystemComponentBase::GiveAbilityWithTag(FGameplayAbilitySpec& Spec)
+{
+	if (const UOneGameplayAbilityBase* GenshinAbility = Cast<UOneGameplayAbilityBase>(Spec.Ability))
+	{
+		if (GenshinAbility->InputTag.IsValid())
+		{
+			Spec.GetDynamicSpecSourceTags().AddTag(GenshinAbility->InputTag);
+		}
+	}
+	return GiveAbility(Spec);
+}
+
+FGameplayAbilitySpecHandle UOneAbilitySystemComponentBase::K2_GiveAbilityWithTag(
+	TSubclassOf<UGameplayAbility> AbilityClass, int32 Level, int32 InputID)
+{
+	// 构建并验证 Ability Spec
+	FGameplayAbilitySpec AbilitySpec = BuildAbilitySpecFromClass(AbilityClass, Level, InputID);
+
+	// 验证类
+	if (!IsValid(AbilitySpec.Ability))
+	{
+		ABILITY_LOG(Error, TEXT("K2_GiveAbility() called with an invalid Ability Class."));
+
+		return FGameplayAbilitySpecHandle();
+	}
+
+	// 授予 ability 并返回 handle。这将运行验证和权限检查
+	return GiveAbilityWithTag(AbilitySpec);
+}
+
+void UOneAbilitySystemComponentBase::AbilityInputTagHeld(const FGameplayTag& InputTag)
+{
+	if (!InputTag.IsValid()) return;
+
+	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())	//循环查找并激活包含有对应InputTag的Ability
+	{
+		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InputTag))
+		{
+			AbilitySpecInputPressed(AbilitySpec);	//通知Ability内的输入按下
+			if (!AbilitySpec.IsActive())
+			{
+				TryActivateAbility(AbilitySpec.Handle);	//并且如果能力未被激活,就激活一次
+			}
+		}
+	}
+}
+
+void UOneAbilitySystemComponentBase::AbilityInputTagReleased(const FGameplayTag& InputTag)
+{
+	if (!InputTag.IsValid()) return;
+
+	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())	//循环查找并激活包含有对应InputTag的Ability
+	{
+		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InputTag))
+		{
+			AbilitySpecInputReleased(AbilitySpec);	//通知Ability内的输入释放
+		}
+	}
 }
 
 
-// Called when the game starts
 void UOneAbilitySystemComponentBase::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
 }
 
 
-// Called every frame
 void UOneAbilitySystemComponentBase::TickComponent(float DeltaTime, ELevelTick TickType,
                                                    FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
--- Source/One/Common/AbilitySystem/OneAbilitySystemComponentBase.h
+++ Source/One/Common/AbilitySystem/OneAbilitySystemComponentBase.h
@@ -16,15 +16,27 @@ class ONE_API UOneAbilitySystemComponentBase : public UAbilitySystemComponent
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UOneAbilitySystemComponentBase();
 
+	// 对传入的Spec添加InputTag后给予技能,这并不是安全的, 因为Spec不是const,需要修改Ability
+	FGameplayAbilitySpecHandle GiveAbilityWithTag(FGameplayAbilitySpec& Spec);
+
+	// 对传入的Spec添加InputTag后给予技能,蓝图方法
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Gameplay Abilities",
+		meta = (DisplayName = "Give Ability With Tag", ScriptName = "GiveAbilityWithTag"))
+	FGameplayAbilitySpecHandle K2_GiveAbilityWithTag(TSubclassOf<UGameplayAbility> AbilityClass, int32 Level = 0,
+	                                                 int32 InputID = -1);
+
+	//从输入调用触发技能的函数
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Abilities")
+	void AbilityInputTagHeld(const FGameplayTag& InputTag);
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Abilities")
+	void AbilityInputTagReleased(const FGameplayTag& InputTag);
+
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 };
