4fed3eb57e3eff75d1839140248b95716fded59e
Z-HongYuan <1939218189@qq.com>
2025-05-25 14:55:33 +0800

    添加作弊管理器 添加蓝图函数库

---
+++ Source/One/Misc/CheatManager/OneCheatManager.h
@@ -0,0 +1,20 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CheatManager.h"
+#include "OneCheatManager.generated.h"
+
+/**
+ * 作弊管理器
+ * 用于在控制台快速执行蓝图函数/命令\n
+ * 比如 在控制台输入 "SetPlayerHealth 5000" 可以执行 当前SetPlayerHealth(5000)函数\n
+ */
+UCLASS()
+class ONE_API UOneCheatManager : public UCheatManager
+{
+	GENERATED_BODY()
+
+public:
+};
---
+++ Source/One/Misc/FunctionLibrary/OneBlueprintFunctionLibrary.h
@@ -0,0 +1,16 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OneBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONE_API UOneBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
