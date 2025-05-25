eeefb30103a6cc324b11e697e5ce9b523f2541db
Z-HongYuan <1939218189@qq.com>
2025-05-25 14:58:25 +0800

    添加GameStateBase 添加PlayerStateBase

---
+++ Source/One/Common/Gameplay/GameState/OneGameStateBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "OneGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONE_API AOneGameStateBase : public AGameState
+{
+	GENERATED_BODY()
+};
---
+++ Source/One/Common/Gameplay/PlayerState/OnePlayerStateBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "OnePlayerStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONE_API AOnePlayerStateBase : public APlayerState
+{
+	GENERATED_BODY()
+};
